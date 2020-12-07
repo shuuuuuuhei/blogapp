381493fe1ee8ee92d698ed42e7f9818e30a51748
shuhei <>
2020-12-07 11:40:15 +0900

    タイムラインのroutesを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :models
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'articles#index'
+  resource :timeline, only: [:show]
 
   resources :articles do
     resources :comments, only: [:new, :create]
