7297d71ff5ba0c9a5d2c422d8172bf83a82a7a55
shuhei <>
2020-12-19 09:18:05 +0900

    turbolinksの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("turbolinks").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -26,6 +27,11 @@ document.addEventListener('turbolinks:load', () => {
     const articleId = dataset.articleId
     axios.get(`/articles/${articleId}/like`)
         .then((response) => {
-            console.log(response)
+            const hasLiked = response.data.hasLiked
+            if (hasLiked) {
+                $('.active-heart').removeClass('hidden')
+            } else {
+                $('.inactive-heart').removeClass('hidden')
+            }
         })
 })
